43266d72a57361f5391dc27bedd12a9a2c4e12de
Brendan Doyle <brendan@webtronic.ie>
2026-02-21 17:48:31 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+## Introduction
+
+Adds a simple VAT / Tax rate field to a menu and exposes it to the API
+
+
+TastyIgniter User extension is open-source software licensed under the [MIT license](https://github.com/igniter-labs/ti-ext-giftup/blob/master/LICENSE.md).
---
+++ src/Extension.php
@@ -0,0 +1,47 @@
+<?php
+namespace WebtronicIE\ProductTax;
+
+use System\Classes\BaseExtension;
+use Admin\Models\Menus_model;
+use Admin\Controllers\Menus;
+
+class Extension extends BaseExtension
+{
+    public function boot()
+    {
+        /*
+         |---------------------------------------------
+         | Extend Menu Model
+         |---------------------------------------------
+         */
+        Menus_model::extend(function ($model) {
+
+            // Allow saving
+            $model->addFillable(['vat_rate']);
+
+            // Make visible in API
+            $model->addVisible(['vat_rate']);
+            $model->makeVisible(['vat_rate']);
+        });
+
+        /*
+         |---------------------------------------------
+         | Add Field To Admin Form
+         |---------------------------------------------
+         */
+        Menus::extendFormFields(function ($form, $model, $context) {
+
+            if (!$model instanceof Menus_model)
+                return;
+
+            $form->addFields([
+                'custom_text' => [
+                    'label' => 'VAT Rate',
+                    'type'  => 'currency',
+                    'span'  => 'full',
+                    'tab'   => 'General',
+                ],
+            ]);
+        });
+    }
+}
