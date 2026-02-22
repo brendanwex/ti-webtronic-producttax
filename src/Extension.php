f7ac118a89cd5cbae79aabdf83b58c803c01e99b
Brendan Doyle <brendan@webtronic.ie>
2026-02-22 10:14:06 +0000

    Bug Fix

--- src/Extension.php
+++ src/Extension.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 namespace WebtronicIE\ProductTax;
 
 use Igniter\System\Classes\BaseExtension;
-//use Igniter\Admin\Models\Menus_model;
 use Admin\Controllers\Menus;
-use Igniter\Cart\Models\Menus_model
+use Igniter\Cart\Models\Menus_model;
 
 class Extension extends BaseExtension
 {
