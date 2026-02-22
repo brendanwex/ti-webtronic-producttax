67bb3bb3d18781dbcea8528bda4e0968471e8aa2
Brendan Doyle <brendan@webtronic.ie>
2026-02-22 19:40:25 +0000

    Bug Fix

--- src/Extension.php
+++ src/Extension.php
@@ -4,55 +4,41 @@ declare(strict_types=1);
 namespace WebtronicIE\ProductTax;
 
 use Igniter\System\Classes\BaseExtension;
-use Igniter\Admin\Controllers\Menus;
-use Igniter\Cart\Models\Menu as Menus_model;
-
+use Igniter\Cart\Models\Menu;
 class Extension extends BaseExtension
 {
 
 
-public function boot()
-{
-    /*
-     |---------------------------------------------
-     | Extend Model
-     |---------------------------------------------
-     */
 
-    Menus_model::extend(function ($model) {
+    public function boot()
+    {
+        // Extend model (this part is correct already)
+        Menu::extend(function ($model) {
 
-        $model->mergeFillable(['vat_rate']);
+            $model->mergeFillable(['vat_rate']);
 
-        $model->mergeCasts([
-            'vat_rate' => 'float',
-        ]);
+            $model->mergeCasts([
+                'vat_rate' => 'float',
+            ]);
 
-        $model->saving(function ($model) {
-            logger('Saving VAT: '.$model->vat_rate);
+            $model->saving(function ($model) {
+                logger('Saving VAT: '.$model->vat_rate);
+            });
         });
-    });
 
+        // Extend backend form config
+        \Event::listen('admin.form.extendConfig', function ($config, $form) {
 
-    /*
-     |---------------------------------------------
-     | Extend Controller Form
-     |---------------------------------------------
-     */
+            if (!$form->model instanceof Menu) {
+                return;
+            }
 
-    Menus::extendFormFields(function ($form, $model, $context) {
-
-        if (!$model instanceof Menus_model) {
-            return;
-        }
-
-        $form->addTabFields([
-            'vat_rate' => [
+            $config->tabs['fields']['vat_rate'] = [
                 'label' => 'VAT Rate',
                 'type'  => 'number',
                 'span'  => 'full',
                 'tab'   => 'tab_general',
-            ],
-        ]);
-    });
-}
+            ];
+        });
+    }
 }
