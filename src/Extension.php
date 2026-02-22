174f8d425eb44dcaecd97f02867bbeae854175c9
Brendan Doyle <brendan@webtronic.ie>
2026-02-22 19:44:44 +0000

    Bug Fix

--- src/Extension.php
+++ src/Extension.php
@@ -4,41 +4,49 @@ declare(strict_types=1);
 namespace WebtronicIE\ProductTax;
 
 use Igniter\System\Classes\BaseExtension;
-use Igniter\Cart\Models\Menu;
+use Igniter\Cart\Models\Menu as Menus_model;
 class Extension extends BaseExtension
 {
 
 
+public function boot()
+{
+    /*
+     |---------------------------------------------
+     | Extend Model
+     |---------------------------------------------
+     */
 
-    public function boot()
-    {
-        // Extend model (this part is correct already)
-        Menu::extend(function ($model) {
+    Menus_model::extend(function ($model) {
 
-            $model->mergeFillable(['vat_rate']);
+        $model->mergeFillable(['vat_rate']);
 
-            $model->mergeCasts([
-                'vat_rate' => 'float',
-            ]);
+        $model->mergeCasts([
+            'vat_rate' => 'float',
+        ]);
 
-            $model->saving(function ($model) {
-                logger('Saving VAT: '.$model->vat_rate);
-            });
+        $model->saving(function ($model) {
+            logger('Saving VAT: '.$model->vat_rate);
         });
+    });
 
-        // Extend backend form config
-        \Event::listen('admin.form.extendConfig', function ($config, $form) {
 
-            if (!$form->model instanceof Menu) {
-                return;
-            }
+    /*
+     |---------------------------------------------
+     | Extend Controller Form
+     |---------------------------------------------
+     */
 
-            $config->tabs['fields']['vat_rate'] = [
+    Menus::extendFormFields(function ($form, $model, $context) {
+
+        $form->addTabFields([
+            'vat_rate' => [
                 'label' => 'VAT Rate',
-                'type'  => 'number',
+                'type'  => 'float',
                 'span'  => 'full',
-                'tab'   => 'tab_general',
-            ];
-        });
-    }
+                'tab'   => 'VAT',
+            ],
+        ]);
+    });
+}
 }
