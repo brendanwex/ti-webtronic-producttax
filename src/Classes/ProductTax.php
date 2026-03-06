2b40cb2bdbe9decfb3e71e5020bb8aa294d13214
Brendan Doyle <brendan@webtronic.ie>
2026-03-06 12:46:22 +0000

    Fix saving of order menu item extra meta fields

--- src/Classes/ProductTax.php
+++ src/Classes/ProductTax.php
@@ -6,7 +6,6 @@ namespace WebtronicIE\ProductTax\Classes;
 use Igniter\Cart\Models\Order;
 use Igniter\Cart\Models\Menu;
 use Igniter\Cart\Models\OrderMenu;
-use Illuminate\Support\Facades\Log;
 
 
 class ProductTax
@@ -21,22 +20,16 @@ class ProductTax
 
             $menuItem = Menu::find($item->menu_id);
 
+            $orderMenuItem = OrderMenu::find($item->order_menu_id);
 
-            $vat_rate = null;
-            $reporting_category = null;
-            $epos_sku = null;
-            if ($menuItem->vat_rate) {
-                $vat_rate = $menuItem->vat_rate;
-            }
-            if ($menuItem->reporting_catgeory) {
-                $reporting_category = $menuItem->reporting_catgeory;
-            }
-            if ($menuItem->epos_sku) {
-                $epos_sku = $menuItem->epos_sku;
-            }
+            $orderMenuItem->vat_rate = $menuItem->vat_rate;
 
+            $orderMenuItem->reporting_category = $menuItem->reporting_category;
 
-            OrderMenu::where('order_menu_id', '=', $item->order_menu_id)->update(['vat_rate' => $vat_rate, 'reporting_category' => $reporting_category, 'epos_sku' => $epos_sku]);
+            $orderMenuItem->epos_sku = $menuItem->epos_sku;
+
+            $orderMenuItem->save();
+
 
         }
 
--- src/Extension.php
+++ src/Extension.php
@@ -97,5 +97,4 @@ class Extension extends BaseExtension
 
 
 
-
 }
