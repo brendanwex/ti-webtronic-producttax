573422e5020932f9478a5817b941377559789fda
Brendan <brendan@webtronic.ie>
2026-03-07 10:21:34 +0000

    Add fields to menu options

---
+++ database/migrations/2026_02_26_000000_add_tax_rate_to_menus_options.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ *
+ */
+return new class extends Migration {
+
+    public function up()
+    {
+        Schema::table('menu_options', function (Blueprint $table) {
+            $table->text('epos_sku')->nullable();
+            $table->text('reporting_category')->nullable();
+            $table->text('vat_rate')->nullable();
+
+        });
+
+        
+    }
+
+    public function down()
+    {
+        Schema::table('menu_options', function (Blueprint $table) {
+            $table->dropColumn('vat_rate');
+            $table->dropColumn('epos_sku');
+            $table->dropColumn('reporting_category');
+
+        });
+    }
+};
